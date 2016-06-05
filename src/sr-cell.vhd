5f5ffcd86f14080d615d9f72b599365be6a4ee99
Parham Alvani <parham.alvani@gmail.com>
2016-06-06 00:47:04 +0430

    Clock sensitive process was added.

--- src/sr-cell.vhd
+++ src/sr-cell.vhd
@@ -10,11 +10,16 @@ use IEEE.std_logic_1164.all;
 entity sr_cell is
 	generic (N : integer := 16)
 	port (left_data, new_data, right_data : in std_logic_vector (N - 1 downto 0);
-		remove, next_empty : in std_logic;
+		clk, remove, next_empty : in std_logic;
 		empty : out std_logic;
 		mux_data : in std_logic_vector (1 downto 0));
 end entity;
 
 architecture rtl of sr_cell is
 begin
+	process (clk)
+	begin
+		if clk'event and clk = '1' then
+		end if;
+	end process;
 end architecture;
