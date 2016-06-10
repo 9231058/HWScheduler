3784f872d713e4993ac53ae092fb3e50d74e1f0d
Parham Alvani <parham.alvani@gmail.com>
2016-06-10 10:30:20 +0430

    Controller module was improved.

--- src/controller.vhd
+++ src/controller.vhd
@@ -8,14 +8,17 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity controller is
+	generic (N : integer := 4);
 	port (enqueue, dequeue : in std_logic;
 		clk, reset : in std_logic;
+		remove, mode : out std_logic;
+		index : out std_logic_vector (N - 1 downto 0);
 		data_in : in std_logic_vector (15 downto 0);
 		data_out : out std_logic_vector (15 downto 0));
 end entity;
 
 architecture rtl of controller is
-	type state is (RST, EQ0, EQ1, DQ0, DQ1);
+	type state is (RST, EQ0, EQ1, DQ0);
 	signal current_state, next_state : state := RST;
 begin
 	process (clk)
@@ -37,6 +40,21 @@ begin
 				else
 					next_state <= RST;
 				end if;
+			when DQ0 =>
+				next_state <= RST;
+		end case;
+	end process;
+	process (current_state)
+	begin
+		case current_state is
+			when RST =>
+				index <= (others <= '0');
+				remove <= '0';
+				mode <= '0';
+			when DQ0 =>
+				index <= (others <= '1');
+				remove <= '1';
+				mode <= '0';
 		end case;
 	end process;
 end;
