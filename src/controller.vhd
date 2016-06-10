130f150cfa95e78bdf561b8011b16911501042e7
Parham Alvani <parham.alvani@gmail.com>
2016-06-10 12:23:32 +0430

    Controller module was improved.

--- src/controller.vhd
+++ src/controller.vhd
@@ -11,8 +11,10 @@ entity controller is
 	generic (N : integer := 4);
 	port (enqueue, dequeue : in std_logic;
 		clk, reset : in std_logic;
+		counter_reset, counter_enable : out std_logic;
 		remove, mode : out std_logic;
 		index : out std_logic_vector (N - 1 downto 0);
+		counter_index : in std_logic_vector (N - 1 downto 0);
 		data_in, tid_in : in std_logic_vector (15 downto 0);
 		data_out, tid_out : out std_logic_vector (15 downto 0));
 end entity;
@@ -20,6 +22,9 @@ end entity;
 architecture rtl of controller is
 	type state is (RST, EQ0, EQ1, DQ0, DQ1);
 	signal current_state, next_state : state := RST;
+
+	signal tid_reg : std_logic_vector (15 downto 0);
+	signal inx_reg : std_logic_vector (N - 1 downto 0);
 begin
 	process (clk)
 	begin
@@ -44,6 +49,10 @@ begin
 				next_state <= DQ1;
 			when DQ1 =>
 				next_state <= RST;
+			when EQ0 =>
+				next_state <= EQ1;
+			when EQ1 =>
+				next_state <= EQ2;
 		end case;
 	end process;
 	process (current_state)
@@ -62,6 +71,17 @@ begin
 				data_out <= tid_in;
 				remove <= '1';
 				mode <= '0';
+			when EQ0 =>
+				counter_reset <= '1';
+				counter_enable <= '1';
+				tid_reg <= data_in;
+			when EQ1 =>
+				counter_reset <= '0';
+				counter_enable <= '1';
+				inx_reg <= counter_index;
+			when EQ2 =>
+				counter_reset <= '0';
+				counter_enable <= '0';
 		end case;
 	end process;
 end;
